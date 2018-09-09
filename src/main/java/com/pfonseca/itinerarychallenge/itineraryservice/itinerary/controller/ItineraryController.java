28fdc6fc8ab186ef2c7fec5999cd529fdcc1cfef
Paulo Vitor Mira Fonseca <paulovitor.java@gmail.com>
2018-09-09 09:57:49 -0300

    Add Swagger documentation in controller parameters

--- src/main/java/com/pfonseca/itinerarychallenge/itineraryservice/itinerary/controller/ItineraryController.java
+++ src/main/java/com/pfonseca/itinerarychallenge/itineraryservice/itinerary/controller/ItineraryController.java
@@ -13,6 +13,8 @@ import com.pfonseca.itinerarychallenge.itineraryservice.itinerary.controller.fil
 import com.pfonseca.itinerarychallenge.itineraryservice.itinerary.domain.Itinerary;
 import com.pfonseca.itinerarychallenge.itineraryservice.itinerary.service.ItineraryService;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
@@ -26,6 +28,11 @@ public class ItineraryController {
 	
 	@GetMapping()
 	@ApiOperation(value="Listing itineraries based on a filter")
+	@ApiImplicitParams({
+	    @ApiImplicitParam(name = "destiny", value = "Destiny city id", required = true, dataType = "long", paramType = "destiny"),
+	    @ApiImplicitParam(name = "origin", value = "Origin city id", required = true, dataType = "long", paramType = "origin"),
+	    @ApiImplicitParam(name = "departureAfter", value = "Departure after. (format: 'HH:mm:ss')", required = true, dataType = "LocalTime", paramType = "departureAfter")
+	})
 	public Page<Itinerary> list(ItineraryFilter filter, Pageable pageable){
 		LOGGER.info("Listing itineraries. Filter: {}", filter);
 		return itineraryService.list(filter, pageable);
